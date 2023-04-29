49f9f1a25462f204f777fa8679f444cb56faed75
rainysteven <rainysteven1@gmail.com>
2023-04-29 21:45:45 +0800

    定义发送邮件的工具类

--- common/common-utils/src/main/java/com/rainy/commonutils/entity/R.java
+++ common/common-utils/src/main/java/com/rainy/commonutils/entity/R.java
@@ -1,5 +1,6 @@
-package com.rainy.commonutils;
+package com.rainy.commonutils.entity;
 
+import com.rainy.commonutils.ResultCode;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
---
+++ common/common-utils/src/main/java/com/rainy/commonutils/serviceImpl/MailServiceImpl.java
@@ -0,0 +1,53 @@
+package com.rainy.commonutils.serviceImpl;
+
+import com.rainy.commonutils.service.MailService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Slf4j
+@Service
+public class MailServiceImpl implements MailService {
+
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+    @Override
+    public void sendSimpleMail(String to, String subject, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(content);
+        mailSender.send(message);
+        log.info("邮件已经发送...");
+    }
+
+    @Override
+    public void sendHtmlMail(String to, String subject, String content) {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper messageHelper;
+        try {
+            messageHelper = new MimeMessageHelper(message, true);
+            messageHelper.setFrom(from);
+            messageHelper.setTo(to);
+            message.setSubject(subject);
+            messageHelper.setText(content, true);
+            mailSender.send(message);
+            log.info("邮件已经发送...");
+        } catch (MessagingException e) {
+            log.error("发送邮件时发生异常！", e);
+        }
+    }
+
+}
