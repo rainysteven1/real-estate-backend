432c20502163012597f0a274085f32c92c4428ea
rainysteven <rainysteven1@gmail.com>
2023-04-29 20:36:07 +0800

    封装接口返回统一信息模版

---
+++ common/common-utils/src/main/java/com/rainy/commonutils/R.java
@@ -0,0 +1,70 @@
+package com.rainy.commonutils;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Data
+public class R {
+
+    // 将json格式中的字段定义为类属性
+    // Swagger注解
+
+    @ApiModelProperty(value = "返回码")
+    private Integer code;
+
+    @ApiModelProperty(value = "返回消息")
+    private String message;
+
+    @ApiModelProperty(value = "返回数据")
+    private Map<String, Object> data = new HashMap<String, Object>();
+
+    //私有化构造器，使其他类只能调用提供的方法
+    private R() {
+
+    }
+
+    // 结果返回成功静态方法
+    public static R success() {
+        R r = new R();
+        r.setCode(ResultCode.SUCCESS);
+        r.setMessage("success");
+        return r;
+    }
+
+    // 结果返回失败静态方法
+    public static R error() {
+        R resultMessage = new R();
+        resultMessage.setCode(ResultCode.ERROR);
+        resultMessage.setMessage("error");
+        return resultMessage;
+    }
+
+
+    // 提供自定义信息的方法，由调用者传入
+    public R message(String message) {
+        this.setMessage(message);
+        return this;
+    }
+
+    public R code(Integer code) {
+        this.setCode(code);
+        return this;
+    }
+
+    public R data(String key, Object value) {
+        this.data.put(key, value);
+        return this;
+    }
+
+    public R data(Map<String, Object> map) {
+        this.setData(map);
+        return this;
+    }
+
+
+}
+
+
---
+++ common/common-utils/src/main/java/com/rainy/commonutils/ResultCode.java
@@ -0,0 +1,16 @@
+package com.rainy.commonutils;
+
+public interface ResultCode {
+    //返回状态成功
+    public static Integer SUCCESS = 200;
+
+    //返回用户模块没有登录权限状态码
+    public static Integer ERROR = 403;
+
+    //返回用户模块登录异常状态码
+    public static Integer ERROR_USER_LOGIN = 10000;
+
+    //返回用户模块注册异常状态码
+    public static Integer ERROR_USER_REGISTER = 10001;
+
+}
