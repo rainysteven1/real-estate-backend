abe375975b70a1a51bf64a7fa2d31787a094d03d
rainysteven <rainysteven1@gmail.com>
2023-04-29 20:36:31 +0800

    封装接口返回统一信息模版

---
+++ service/service-user/src/main/java/com/rainy/service_user/serviceImpl/UserinfoServiceImpl.java
@@ -0,0 +1,20 @@
+package com.rainy.service_user.serviceImpl;
+
+import com.rainy.service_user.entity.Userinfo;
+import com.rainy.service_user.mapper.UserinfoMapper;
+import com.rainy.service_user.service.UserinfoService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author rainy
+ * @since 2023-04-29
+ */
+@Service
+public class UserinfoServiceImpl extends ServiceImpl<UserinfoMapper, Userinfo> implements UserinfoService {
+
+}
