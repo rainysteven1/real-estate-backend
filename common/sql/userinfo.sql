a754fff467bd88e7eca5727a4b38c6da0344c737
rainysteven <rainysteven1@gmail.com>
2023-04-29 17:45:14 +0800

    修改userinfo.sql文件和代码生成类

--- common/sql/userinfo.sql
+++ common/sql/userinfo.sql
@@ -1,12 +1,12 @@
 CREATE TABLE `userinfo`
 (
-    `id`       int                    NOT NULL AUTO_INCREMENT COMMENT '用户ID',
-    `username` varchar(30)            NOT NULL COMMENT '用户名',
-    `password` varchar(50)            NOT NULL COMMENT '密码',
-    `mail`     varchar(50)            NOT NULL COMMENT '用户邮箱',
-    `avatar`   varchar(255)           NOT NULL COMMENT '用户头像',
-    `created`  DATE                   NOT NULL COMMENT '创建时间',
-    `modified` DATE                   NOT NULL COMMENT '修改时间',
-    `deleted`  tinyint(1) default (0) NOT NULL COMMENT '是否删除',
+    `id`       INT                 NOT NULL AUTO_INCREMENT COMMENT '用户ID',
+    `username` VARCHAR(30)         NOT NULL COMMENT '用户名',
+    `password` VARCHAR(50)         NOT NULL COMMENT '密码',
+    `mail`     VARCHAR(50)         NOT NULL COMMENT '用户邮箱',
+    `avatar`   VARCHAR(255)        NOT NULL COMMENT '用户头像',
+    `created`  DATETIME            NOT NULL COMMENT '创建时间',
+    `modified` DATETIME            NOT NULL COMMENT '修改时间',
+    `deleted`  TINYINT(1) UNSIGNED NOT NULL DEFAULT (0) COMMENT '逻辑删除1已删除,0未删除',
     PRIMARY KEY (`id`)
 );
---
+++ service/service-user/src/test/java/com/rainy/test/auto_code/CodeGenerator.java
@@ -0,0 +1,9 @@
+package com.rainy.test.auto_code;
+
+import org.junit.Test;
+
+public class CodeGenerator {
+    @Test
+    public void run() {
+    }
+}
