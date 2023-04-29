3e21867bcd01bb1c9352bd8b9add5a0438854aef
rainysteven <rainysteven1@gmail.com>
2023-04-29 18:06:40 +0800

    修改项目pom文件

--- service/service-user/src/test/java/com/rainy/test/auto_code/CodeGenerator.java
+++ service/service-user/src/test/java/com/rainy/test/auto_code/CodeGenerator.java
@@ -5,5 +5,16 @@ import org.junit.Test;
 public class CodeGenerator {
     @Test
     public void run() {
+        // 数据库配置
+        String dbURL = "jdbc:mysql://localhost:13306/tb_userinfo?useUnicode=true&characterEncoding=UTF-8&serverTimezone=GMT%2B8";
+        String dbUsername = "root";
+        String dbPassword = "123456";
+
+        // 当前项目绝对路径
+        String projectPath = System.getProperty("user.dir");
+        // 代码输出目录
+        String outputDir = projectPath + "/src/main/java";
+        System.out.println(outputDir);
+
     }
 }
